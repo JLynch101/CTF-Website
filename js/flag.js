dfe363b6356f2972ca7f9fd4d1acc99c2bad38c5
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-03-11 16:17:22 +0000

    Added sample challenges

---
+++ js/flag.js
@@ -0,0 +1,19 @@
+function z() {
+    var a = document.getElementById("f1input");
+    if ((a.value == "KDAQDQ5552DQ") || (a.value == "KDAQDQ5552DQ")) {
+        document.getElementById('f1').innerHTML = '  Correct flag, Well Done!'.fontcolor("green");
+    } else {
+        document.getElementById('f1').innerHTML = '	Incorrect flag! '.fontcolor("red");
+
+    }
+}
+
+function x() {
+    var b = document.getElementById("f2input");
+    if ((b.value == "JQ71321PDDUH") || (b.value == "JQ71321PDDUH")) {
+        document.getElementById('f2').innerHTML = '  Correct flag, Well Done!'.fontcolor("green");
+    } else {
+        document.getElementById('f2').innerHTML = '	Incorrect flag! '.fontcolor("red");
+
+    }
+}
